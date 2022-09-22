4b05b5de0f3eccc0bd70e9ac545a72a3f947556d
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:48:12 +0530

    Add files via upload

---
+++ ifelse advance.java
@@ -0,0 +1,15 @@
+//Auther: praveen kumar
+//Title: ifelse advance
+//Date: 11/08/2022
+class ifelse_advance
+{
+ public static void main(String[]args)
+ {
+  int i=50;
+  float j=7.5F;
+   {
+    String output= (i<=10)?"defined output":"undefined output";
+    String statement= (j>=10.5)?"true statement":"false statement";
+   }
+  }
+}
