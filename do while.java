6780796c8da0c6ff015dab76e1f0598af02260f4
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:30:58 +0530

    Add files via upload

---
+++ do while.java
@@ -0,0 +1,14 @@
+//Auther: Praveen kumar
+//Title: do While
+//Date: 06/08/2022
+class do_while
+{
+ public static void main(String[]args)
+ {
+ int j=15;
+ do
+  {
+   System.out.println("hello i am dowhile");
+  }while(j<=10);
+  }
+}
