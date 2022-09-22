0a750b76866aed9e50b0f9ac5ed261ca4783443f
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:35:22 +0530

    Add files via upload

---
+++ for loop.java
@@ -0,0 +1,18 @@
+//Auther: Praveen kumar
+//Title: Enhanced for
+//Date: 07/08/2022
+class for_loop
+{
+ public static void main(String[]args)
+ {
+  int i=10;
+  for(;;)
+   {
+    if(i<=15)
+     {
+      System.out.println("hello i am advanced for loop");
+      i++;
+     }
+   }
+  }
+}
