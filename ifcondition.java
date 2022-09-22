96cb60eb292906c520df3ed9ba6b0f3bf57e2ec2
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:44:18 +0530

    Add files via upload

---
+++ ifcondition.java
@@ -0,0 +1,16 @@
+//Auther: praveenkumar
+//Title: if 
+//Date: 11/08/2022
+
+
+class ifcondition
+{
+ public static void main(String[]args)
+ {
+ int i=10;
+ if(i<=10)
+  {
+    System.out.print("output is successfully executed");
+  }
+ }
+}  
