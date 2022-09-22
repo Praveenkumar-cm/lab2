478c6cc00c41b080de309545913f872cd1639c0c
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:36:53 +0530

    Add files via upload

---
+++ else if.java
@@ -0,0 +1,26 @@
+//Auther: Praveen kumar
+//Title: else if
+//Date:06/08/2022
+class else_if
+{
+ public static void main(String[]args)
+ {
+  char a='z';
+  boolean b=true;
+  double c=17.45;
+  {
+   if(a<='Z')
+    {
+     System.out.println("Alphabet");
+    }
+   else if(b==false)
+    {
+      System.out.println("true or false");
+    }
+   else if(c<=17.45)
+    {
+      System.out.println("hey am i here");
+    }
+   }
+  }
+}
