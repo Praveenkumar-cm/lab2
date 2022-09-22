1484cdb6cd5679e814fd1246182a15802a129ace
Praveenkumar-cm <110027914+Praveenkumar-cm@users.noreply.github.com>
2022-09-22 15:41:18 +0530

    Add files via upload

---
+++ for statement.java
@@ -0,0 +1,14 @@
+//Auther: praveen kumar
+//Title: for
+//Date: 07/08/2022
+class for_statement
+{
+ public static void main(String[]args)
+ {
+  int i;
+  for(i=5;i<=10;i++)
+   {
+     System.out.println(i);
+   }
+ }
+}   
